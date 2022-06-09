306b9298e2b057a307f9d9211dbeb3be82a3669e
zzk72 <103580430+zzk72@users.noreply.github.com>
2022-06-09 09:26:39 +0800

    Update 序列.cpp

--- 2/序列.cpp
+++ 2/序列.cpp
@@ -1,3 +1,38 @@
+/*
+7-4 序列乘积
+分数 100
+作者 谷方明
+单位 吉林大学
+两个递增序列A和B,长度都是n。令 Ai 和 Bj 做乘积，1≤i,j≤n.请输出n*n个乘积中从小到大的前n个。
+
+输入格式:
+第1行，1个整数n，表示序列的长度， 1≤n≤100000.
+
+第2行，n个整数Ai，用空格分隔，表示序列A，1≤Ai≤40000，1≤i≤n.
+
+第3行，n个整数Bi，用空格分隔，表示序列B，1≤Bi≤40000，1≤i≤n.
+
+输出格式:
+1行，n个整数，用空格分隔，表示序列乘积中的从小到大前n个。
+
+输入样例:
+在这里给出一组输入。例如：
+
+5
+1 3 5 7 9 
+2 4 6 8 10
+输出样例:
+在这里给出相应的输出。例如：
+
+2 4 6 6 8
+代码长度限制
+16 KB
+时间限制
+100 ms
+内存限制
+5 MB
+*/
+
 #include<iostream>
 #include<queue>
 #include<vector>
@@ -38,7 +73,7 @@ int main() {
         if (i < n - 1)printf("%d ", tmp.data);
         else printf("%d\n", tmp.data);
             node tmp2;
-            tmp2.x = tmp.x + 1; tmp2.y = tmp.y; tmp2.data = a[tmp2.x] * b[tmp2.y];//�������һ�������
+            tmp2.x = tmp.x + 1; tmp2.y = tmp.y; tmp2.data = a[tmp2.x] * b[tmp2.y];//½«ÆäµÝÔöÒ»´ÎÔÙÈë¶Ó
             PQ.push(tmp2);
     }
 
